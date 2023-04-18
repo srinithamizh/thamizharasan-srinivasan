2ad5f87b97b1920ee970b629513c32cb91b65704
Thamizharasan Srinivasan <srinithamizh@gmail.com>
2023-04-18 12:37:56 +0530

    portfolio initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Thamizharasan Srinivasan';
-  designation = 'Software Developer';
+  designation = 'Software Development Engineer';
 }
