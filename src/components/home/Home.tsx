519374a3f01f7df7d3ee4211f5fcdc81e0cba852
Thamizharasan Srinivasan <srinithamizh@gmail.com>
2023-12-25 12:16:04 +0530

    Add: Description in Home Component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,16 +1,23 @@
 import logo from '../../assets/logo.png'
+import Utils from '../../utils/utils';
 import './Home.css'
 
 export default function Home() {
     const name: string = 'Thamizharasan Srinivasan';
     const role: string = 'Software Developer';
+    const experience: number = Utils.calculateExperience();
+    const description: string = `I'm a ${role} with ${experience}+ years of professional experience in Java, Spring Boot and
+    Angular. Active Team player and proven ability to approach problems effectively, prioritize and complete
+    multiple task. Interested in learning new technologies.
+    `;
 
     return (
         <>
             <section id="home">
                 <img src={logo} className="logo" alt={name} />
                 <h1 className="title">Hello, I'm <span className="name">{name}</span></h1>
-                <h2 className="role">{role}</h2>
+                <h4 className="role">{description}</h4>
+                <button className="button scrollButton">Scroll for more</button>
             </section>
         </>
     );
---
+++ src/utils/utils.tsx
@@ -0,0 +1,16 @@
+
+function calculateExperience(): number {
+    const currentDate = new Date();
+    const DOJ = new Date(2020, 8);
+    let differeceInYears: number = currentDate.getFullYear() - DOJ.getFullYear();
+    if (
+        currentDate.getMonth() < DOJ.getMonth() ||
+        (currentDate.getMonth() === DOJ.getMonth() &&
+            currentDate.getDate() < DOJ.getDate())
+    ) {
+        differeceInYears--;
+    }
+    return differeceInYears;
+}
+
+export default { calculateExperience }
